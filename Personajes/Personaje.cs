b326d242288149244fabf57089d956af28fa3f8a
Gabi <alagastinofabio@gmail.com>
2026-02-09 12:58:52 -0300

    Solucion a error en armadura y resistencia magica

--- Personajes/Personaje.cs
+++ Personajes/Personaje.cs
@@ -1,54 +1,65 @@
 namespace jueguito.Personajes
 {
     public class Personaje
-{
-    public string Nombre { get; set; }
-    public int Vida { get; protected set; }
-    public int Armadura { get; protected set; }
-    public int ResistenciaMagica { get; protected set; }
-
-    public Personaje(string nombre)
     {
-        Nombre = nombre;
-        Vida = 100;
-        Armadura = 0;
-        ResistenciaMagica = 0;
-    }
+        public string Nombre { get; set; }
+        public int Vida { get; protected set; }
+        public int Armadura { get; protected set; }
+        public int ResistenciaMagica { get; protected set; }
 
-    public void RecibirDañoMagico(int cantidad)
-    {
-        int dañoFinal = cantidad - (cantidad / 100) * ResistenciaMagica;
-        Vida -= dañoFinal;
-        if (Vida < 1)
+        public Personaje(string nombre)
+        {
+            Nombre = nombre;
+            Vida = 100;
+            Armadura = 0;
+            ResistenciaMagica = 0;
+        }
+
+        public void RecibirDañoMagico(int cantidad)
+        {
+            int dañoFinal = cantidad - (cantidad * ResistenciaMagica) / 100;
+            if (dañoFinal < 1)
+            {
+                dañoFinal = 1;
+            }
+
+            Vida -= dañoFinal;
+
+            if (Vida < 1)
             {
                 Vida = 0;
             }
-        Console.WriteLine($"{Nombre} ha recibido {cantidad} de daño magico.");
-    }
+            Console.WriteLine($"{Nombre} ha recibido {dañoFinal} de daño magico.");
+        }
 
-    public void RecibirDañoFisico(int cantidad)
-    {
-        int dañoFinal = cantidad - (cantidad / 100) * Armadura;
-        Vida -= dañoFinal;
-        if (Vida < 1)
+        public void RecibirDañoFisico(int cantidad)
+        {
+            int dañoFinal = cantidad - (cantidad * Armadura) / 100;
+            if (dañoFinal < 1)
+            {
+                dañoFinal = 1;
+            }
+            Vida -= dañoFinal;
+
+            if (Vida < 1)
             {
                 Vida = 0;
             }
-        Console.WriteLine($"{Nombre} ha recibido {cantidad} de daño Fisico");
-    }
+            Console.WriteLine($"{Nombre} ha recibido {dañoFinal} de daño Fisico");
+        }
 
-    public void VidaActual(Personaje objetivo)
-    {
-        Console.WriteLine($"A {objetivo.Nombre} le quedan {objetivo.Vida} puntos de vida.");
-    }
+        public void VidaActual(Personaje objetivo)
+        {
+            Console.WriteLine($"A {objetivo.Nombre} le quedan {objetivo.Vida} puntos de vida.");
+        }
 
-    public virtual void Atacar(Personaje objetivo)
-    {
-        Console.WriteLine($"{Nombre} ha lanzado un ataque a {objetivo.Nombre}");
-        objetivo.RecibirDañoFisico(10);
-        objetivo.RecibirDañoMagico(0);
-        VidaActual(objetivo);
+        public virtual void Atacar(Personaje objetivo)
+        {
+            Console.WriteLine($"{Nombre} ha lanzado un ataque a {objetivo.Nombre}");
+            objetivo.RecibirDañoFisico(10);
+            objetivo.RecibirDañoMagico(0);
+            VidaActual(objetivo);
+        }
     }
-}
 
 }
